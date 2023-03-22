c29f62f091289d851db24897d019adb9c5f550b0
Jose Rojas <jgra11.2010@gmail.com>
2023-03-22 15:20:22 -0400

    Larri IA beta-1.8

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,28 +11,24 @@ export default function MyApp({ Component, pageProps }: AppProps<{}>) {
 				<link rel='icon' href='/favicon.png' />
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap'
-					rel='stylesheet'
-				/>
+			</Head>
 
-				{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-				<Script
-					src='https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID'
-					strategy='afterInteractive'
-				/>
-				<Script id='google-analytics' strategy='afterInteractive'>
-					{`
+			{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+			<Script
+				src='https://www.googletagmanager.com/gtag/js?id=G-3ZVBJ60L83'
+				strategy='afterInteractive'
+			/>
+			<Script id='google-analytics' strategy='afterInteractive'>
+				{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+          gtag('config', 'G-3ZVBJ60L83', {
 			page_path: window.location.pathname,
 		  });
         `}
-				</Script>
-			</Head>
+			</Script>
 			<Component {...pageProps} />
 		</>
 	)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head />
+			<link
+				href='https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap'
+				rel='stylesheet'
+			/>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
